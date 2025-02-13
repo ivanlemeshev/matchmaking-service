9816af4a6a6de37b4c15e8d9601265cfd21d8ce5
Ivan Lemeshev <vanlmshv@gmail.com>
2025-02-13 15:40:19 +0200

    Add found match notifier tests

--- internal/matchmaking/matchmaker/found_match_notifier_test.go
+++ internal/matchmaking/matchmaker/found_match_notifier_test.go
@@ -1,6 +1,70 @@
 package matchmaker
 
-import "testing"
+import (
+	"sync"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestFoundMatchNotifier(t *testing.T) {
+	notifier := newFoundMatchNotifier()
+
+	var result1, result2, result3 chan *Match
+
+	t.Run("should not have listeners on creation", func(t *testing.T) {
+		assert.NotNil(t, notifier)
+		assert.Equal(t, 0, len(notifier.listeners))
+	})
+
+	t.Run("should register listeners", func(t *testing.T) {
+		result1 = notifier.registerListener("1")
+		assert.Equal(t, 1, len(notifier.listeners))
+
+		result2 = notifier.registerListener("2")
+		assert.Equal(t, 2, len(notifier.listeners))
+
+		result3 = notifier.registerListener("3")
+		assert.Equal(t, 3, len(notifier.listeners))
+	})
+
+	t.Run("should notify listeners", func(t *testing.T) {
+		matchID := int64(123)
+		match := Match{ID: matchID}
+
+		wg := sync.WaitGroup{}
+
+		wg.Add(3)
+		go func() {
+			defer wg.Done()
+			assert.Equal(t, matchID, (<-result1).ID)
+		}()
+
+		go func() {
+			defer wg.Done()
+			assert.Equal(t, matchID, (<-result2).ID)
+		}()
+
+		go func() {
+			defer wg.Done()
+			assert.Equal(t, matchID, (<-result3).ID)
+		}()
+
+		notifier.notify("1", &match)
+		notifier.notify("2", &match)
+		notifier.notify("3", &match)
+
+		wg.Wait()
+	})
+
+	t.Run("should unregister listeners", func(t *testing.T) {
+		notifier.unregisterListener("1")
+		assert.Equal(t, 2, len(notifier.listeners))
+
+		notifier.unregisterListener("2")
+		assert.Equal(t, 1, len(notifier.listeners))
+
+		notifier.unregisterListener("3")
+		assert.Equal(t, 0, len(notifier.listeners))
+	})
 }
